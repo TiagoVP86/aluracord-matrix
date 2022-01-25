f22e18479be1f3113d5446d94aad5e6b0189367e
Tiago <t.vp@hotmail.com>
2022-01-25 10:48:21 -0300

    Segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import {Box, Button, Text, TextField, Image} from '@skynexui/components';
+import React from 'react';
+import {useRouter} from 'next/router';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    );
-  }
     
 function Titulo(props) {
     console.log(props);
@@ -60,11 +33,12 @@ function Titulo(props) {
   export default HomePage*/
 
   export default function PaginaInicial() {
-    const username = 'TiagoVP86';
+    //const username = 'TiagoVP86';
+    const [username, setUsername] = React.useState('TiagoVP86');
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +65,10 @@ function Titulo(props) {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function(event){
+                event.preventDefault();
+                roteamento.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -100,8 +78,29 @@ function Titulo(props) {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+
+              {/*<input 
+                type="text"
+                value={username}
+                onChange={function(event){
+                  console.log('usuario digitou', event.target.value);
+                  //Onde ta o valor?
+                  const valor = event.target.value;
+                  //Trocar o valor da variavel
+                  setUsername(valor);
+                }}
+              />*/}
+
               <TextField
+                 value={username}
+                 onChange={function(event){
+                   console.log('usuario digitou', event.target.value);
+                   //Onde ta o valor?
+                   const valor = event.target.value;
+                   //Trocar o valor da variavel
+                   setUsername(valor);
+                 }}
+
                 fullWidth
                 textFieldColors={{
                   neutral: {
